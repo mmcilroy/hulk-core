94f79a36b4364e1386d6f320bbcaa416bfd5a4d0
mmcilroy <mmcilroy@hulk-01d.lan>
2013-08-08 23:10:42 +0100

    better logging

--- include/hulk/core/logger.h
+++ include/hulk/core/logger.h
@@ -1,10 +1,78 @@
+
 #ifndef _hulk_core_logger_h_
 #define _hulk_core_logger_h_
 
+#include <map>
+#include <sstream>
 #include <iostream>
+#include <cstring>
 
-#define LOG_DEBUG( msg ) std::cerr << "DBG " << __FILE__ << ":" << __LINE__ << " - " << msg << std::endl
-#define LOG_INFO( msg )  std::cerr << "INF " << __FILE__ << ":" << __LINE__ << " - " << msg << std::endl
-#define LOG_ERROR( msg ) std::cerr << "ERR " << __FILE__ << ":" << __LINE__ << " - " << msg << std::endl
+namespace hulk {
+namespace core {
+
+// -----------------------------------------------------------------------------
+#define LOG_DEBUG( l, m ) std::stringstream ss; ss << "DBG " << filename( __FILE__ ) << ":" << __LINE__ << " - " << m; l.write( log::DEBUG, ss.str() );
+#define LOG_INFO( l, m )  std::stringstream ss; ss << "INF " << filename( __FILE__ ) << ":" << __LINE__ << " - " << m; l.write( log::INFO, ss.str() );
+#define LOG_ERROR( l, m ) std::stringstream ss; ss << "ERR " << filename( __FILE__ ) << ":" << __LINE__ << " - " << m; l.write( log::ERROR, ss.str() );
+
+inline const char* filename( const char* s ) {
+    return strrchr( s, '/' ) ? strrchr( s, '/' ) + 1 : s;
+}
+
+// -----------------------------------------------------------------------------
+class log
+{
+public:
+    typedef enum {
+        DEBUG, INFO, WARN, ERROR
+    } level;
+
+    log( level l ) {
+        _level = l;
+    }
+
+    virtual void write( level l, const std::string& s ) {
+        if( l >= _level ) std::cout << s << std::endl;
+    }
+
+private:
+    level _level;
+};
+
+// -----------------------------------------------------------------------------
+class logger
+{
+public:
+    static logger& instance() {
+        if( !_instance ) {
+            _instance = new logger;
+        }
+        return *_instance;
+    }
+
+    log& get( const std::string& id ) {
+        logmap::const_iterator it = _logs.find( id );
+        if( it == _logs.end() ) {
+            return _default_log;
+        } else {
+            return *it->second;
+        }
+    }
+
+private:
+    logger() : _default_log( log::DEBUG ) {}
+
+    typedef std::map< std::string, log* > logmap;
+
+    log _default_log;
+    logmap _logs;
+
+    static logger* _instance;
+};
+
+logger* logger::_instance = 0;
+
+}
+}
 
 #endif
--- test/echo-server.cpp
+++ test/echo-server.cpp
@@ -1,22 +1,23 @@
 
 #include "hulk/core/tcp.h"
-
-#include <iostream>
+#include "hulk/core/logger.h"
 
 using namespace hulk::core;
+
+log& l = logger::instance().get( "hulk.core.test" );
 
 struct echo_callback : public tcp::callback
 {
     void on_open( int fd ) {
-        std::cout << "on_open: " << fd << std::endl;
+        LOG_INFO( l, "on_open: " << fd );
     }
 
     void on_close( int fd ) {
-        std::cout << "on_close: " << fd << std::endl;
+        LOG_INFO( l, "on_close: " << fd );
     }
 
     void on_recv( int fd, const char* data, size_t len ) {
-        std::cout << "on_recv: " << fd << " " << len << std::endl;
+        LOG_INFO( l, "on_recv: " << fd << " " << len );
     }
 };
 
@@ -24,7 +25,7 @@ int main( int argc, char** argv )
 {
     echo_callback cb;
     tcp::event_loop eloop( 1024, 5000, cb );
-    eloop.watch( tcp::bind( 5555, 1024 ), true );
+    eloop.watch( tcp::bind( 5557, 1024 ), true );
 
     while( 1 ) {
         eloop.loop();
