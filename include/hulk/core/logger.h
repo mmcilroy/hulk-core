3af6cdca0a571d19ccf8412eb31d55c3735ce393
mmcilroy <m1rk.mci@gmail.com>
2013-09-11 01:41:33 +0100

    prefix local log vars with underscores

--- include/hulk/core/logger.h
+++ include/hulk/core/logger.h
@@ -11,9 +11,9 @@ namespace hulk {
 namespace core {
 
 // -----------------------------------------------------------------------------
-#define LOG_DEBUG( l, m ) { std::stringstream ss; ss << std::endl << "DBG " << ::hulk::core::filename( __FILE__ ) << ":" << __LINE__ << " - " << m; l.write( ::hulk::core::log::DEBUG, ss.str() ); }
-#define LOG_INFO( l, m )  { std::stringstream ss; ss << std::endl << "INF " << ::hulk::core::filename( __FILE__ ) << ":" << __LINE__ << " - " << m; l.write( ::hulk::core::log::INFO, ss.str() );  }
-#define LOG_ERROR( l, m ) { std::stringstream ss; ss << std::endl << "ERR " << ::hulk::core::filename( __FILE__ ) << ":" << __LINE__ << " - " << m; l.write( ::hulk::core::log::ERROR, ss.str() ); }
+#define LOG_DEBUG( l, m ) { std::stringstream __ss; __ss << std::endl << "DBG " << ::hulk::core::filename( __FILE__ ) << ":" << __LINE__ << " - " << m; l.write( ::hulk::core::log::DEBUG, __ss.str() ); }
+#define LOG_INFO( l, m )  { std::stringstream __ss; __ss << std::endl << "INF " << ::hulk::core::filename( __FILE__ ) << ":" << __LINE__ << " - " << m; l.write( ::hulk::core::log::INFO, __ss.str() );  }
+#define LOG_ERROR( l, m ) { std::stringstream __ss; __ss << std::endl << "ERR " << ::hulk::core::filename( __FILE__ ) << ":" << __LINE__ << " - " << m; l.write( ::hulk::core::log::ERROR, __ss.str() ); }
 
 inline const char* filename( const char* s ) {
     return strrchr( s, '/' ) ? strrchr( s, '/' ) + 1 : s;
