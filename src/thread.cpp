9d0764be4835d1e281bcca52e4a90ac2f30c3097
mmcilroy <m1rk.mci@gmail.com>
2013-11-26 17:03:28 +0000

    broke the build :(

--- src/thread.cpp
+++ src/thread.cpp
@@ -11,7 +11,9 @@ void sleep_ms( int ms )
     struct timespec t, r;
     t.tv_sec = ms / 1000;
     t.tv_nsec = ( ms % 1000 ) * 1000;
-    nanosleep( &t, &r ) == -1 );
+    if( nanosleep( &t, &r ) == -1 ) {
+        perror( "sleep_ms failed" );
+    }
 }
 
 }
